a4af860a709212916d9a29dc93c470c52ba2fe57
bangjun298-ctrl <bangjun298@gmail.com>
2025-09-16 17:42:39 +0900

    9/16 update

--- queue/circular_queue_class.py
+++ queue/circular_queue_class.py
@@ -10,8 +10,20 @@ class CircularQueueOneSlotEmpty:
     - rear:  '맨 마지막 요소'의 인덱스
     """
     
-    
+    def __init__(self,capacity):
+        self.capacity = capacity
+        self.N = self.capacity + 1 # 내부 배열 크기 (단, 배열의 한칸(슬롯)비용)
+        self.array = [None] * self.N
+        self.front = 0
+        self.rear = 0
 
+    def is_empty(self):
+        return self.front == self.rear
+
+    def is_full(self):
+        return self.front == (self.rear + 1) % self.N
+
+    
     
    
     def display(self, msg="CircularQueueOneSlotEmpty"):
