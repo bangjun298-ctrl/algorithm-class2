063bb5bc2259ec909b108592755b48592695dbe1
jojunhwan03 <jjo525029@gmail.com>
2025-11-18 17:34:44 +0900

    11/18 추가

--- 11월11일/graph_tues.py
+++ 11월11일/graph_tues.py
@@ -12,8 +12,6 @@ def DFS(vtx, edge, s, visited):
         if adj[s][v] != 0 and not visited[v]: # 간선이 있고, 방문하지 않았다면
             DFS(vtx, adj, v, visited) # 그 정점으로 이동하여 DFS 재귀 함수 호출
 
-
-
 # -------------------------------
 # 테스트 코드
 # -------------------------------
@@ -26,8 +24,7 @@ if __name__ == "__main__":
         [1, 0, 1, 1, 0],     # V -> U, W, X
         [1, 1, 0, 0, 1],     # W -> U, V, Y
         [0, 1, 0, 0, 1],     # X -> V
-        [0, 0, 1, 0, 0]      # Y -> W
-        ]
+        [0, 0, 1, 0, 0]]      # Y -> W
 
     print('DFS(출발: U) : ', end="")
     visited = [False] * len(vtx)   # 방문 여부 리스트 초기화
@@ -35,27 +32,41 @@ if __name__ == "__main__":
 
 
 # 2. 너비 우선 탐색 (BFS) - 인접 리스트 사용
- 
+from collections import deque # FIFO 큐
+def BFS_AL(vtx, alist, s):
+    n = len(vtx)        # 정점의 개수
+    visited = [False]*n # 방문여부 기록
+    q = deque()         # 큐 생성
 
+    q.append(s)         # 시작 정점을 큐에 삽입
+    visited[s] = True   # 방문 표시
 
+    while q :                    # 큐가 빌 때까지 반복
+        s = q.popleft()          # 큐의 맨 앞의 정점 꺼내기
+        print(vtx[s], end = ' ') # 방문 정점 출력
+
+        # s의 인접 정점들 순회
+        for v in alist[s]:
+            if not visited[v]:
+                q.append(v)        # 방문하지 않은 정점을 큐에 삽입
+                visited[v] = True  # 방문 표시
 # -------------------------------
 # 테스트 코드
 # -------------------------------
-if __name__ == "__main__":
+#if __name__ == "__main__":
     # 정점 리스트
-    vtx = ['U', 'V', 'W', 'X', 'Y']
+#    vtx = ['U', 'V', 'W', 'X', 'Y']
     # 인접 리스트 
-    alist = [
-        [1, 2],       # U -> V, W
-        [0, 2, 3],    # V -> U, W, X
-        [0, 1, 4],    # W -> U, V, Y
-        [1],          # X -> V
-        [2]           # Y -> W
-    ] 
+#    alist = [
+     #   [1, 2],       # U -> V, W
+    #    [0, 2, 3],    # V -> U, W, X
+   #     [0, 1, 4],    # W -> U, V, Y
+  #      [1],          # X -> V
+ #       [2]           # Y -> W
+#    ] 
 
-    print('BFS_AL(출발: U) : ', end="")
-    BFS_AL(vtx, alist, 0)  # 시작점 U는 인덱스 0
-
+ #   print('BFS_AL(출발: U) : ', end="")
+ #   BFS_AL(vtx, alist, 0)  # 시작점 U는 인덱스 0
 
 
 # 3. DFS를 이용한 신장트리 생성 (인접행렬 방식)
@@ -65,22 +76,57 @@ if __name__ == "__main__":
 # -------------------------------
 # 테스트 코드
 # -------------------------------
-if __name__ == "__main__":
+#if __name__ == "__main__":
     # 정점 리스트
 
     # 인접 행렬 
     
 
-    print("ST_DFS_AM : ", end="")
-    visited = [False] * len(vtx)
-    ST_DFS(vtx, adj, 0, visited)   # U에서 시작
+#    print("ST_DFS_AM : ", end="")
+ #   visited = [False] * len(vtx)
+#    ST_DFS(vtx, adj, 0, visited)   # U에서 시작
 
 
 # 4. Prim 알고리즘 (인접행렬 방식)
+# getMinVertex()를 내부 함수로 정의
+# INF = 999 => 간선이 없음을 의미
+def  MSTPrim(vertex, adj, INF = 999):
+    n = len(vertex)
+    dist = [INF] * n       # 각 정점까지의 최소 거리
+    selected = [False] * n # MST 의 정점 포함 여부 기록
+    dist[0] = 0            # 시작 정점의 거리 = 0
 
+    # helper 함수 정의
+    def getMinVertex(dist, selected):
+        # MST에 포함되지 않은 정점 중 최소 dist를 가진 정점을 반환
+        # 매번 전체 정점을 선형탐색 찾기
+        minv = -1
+        mindist = INF
+        for v in range(len(dist)):
+            if not selected[v] and dist[v] < mindist:
+                mindist = dist[v]
+                minv = v
+        return minv
+    
+    print("정점 추가 순서:", end=' ')
+    print()
+    for _ in range(n):
+        u = getMinVertex(dist, selected)
 
+        selected[u] = True
+        print(vertex[u],"(",dist[u],")", end=' ')  # MST에 추가된 정점 출력
 
- 
+        # 거리 갱신: 그 정점과 연결된 간선으로 다른 정점들의 거리(dist) 갱신
+        for v in range(n):
+            if adj[u][v] != INF and not selected[v]:
+                if adj[u][v] < dist[v]:
+                    dist[v] = adj[u][v]
+            print("|", vertex[u], "-", vertex[v], "=>", dist[v], end='')
+
+        print(": ", dist) # 중간 dist 출력
+
+    print("\n최종 dist 배열:", dist)
+    print("MST 총 비용:", sum(dist))
 
 # ---------------------------------------------------
 # 테스트 코드
